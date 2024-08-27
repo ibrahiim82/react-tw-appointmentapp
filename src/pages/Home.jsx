cd9cdda4d3cead5c41ff9d8483b80cca2d6c0c39
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-08-27 21:19:37 +0300

    lastcommit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,24 @@ import { Doctors } from "../components/Doctors";
 import { AppointmentList } from "../components/AppointmentList";
 
 export const Home = () => {
-  const [appointments , setAppointments] = useState(appointmentData)
-  
-  const handleAddApp = (newAppointment) => {
-    setAppointments([...appointments, newAppointment])
-  }
+  const [appointments, setAppointments] = useState(appointmentData);
 
-//   event yakaladığımız içine uniq bir ifadeyi yakalamamız gerekiyor o yüzden id gerekli
+  const handleAddApp = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+  };
+
+  //   event yakaladığımız içine uniq bir ifadeyi yakalamamız gerekiyor o yüzden id gerekli
   const handleDoubleClick = (id) => {
-    const updateList = appointments.map((item) => item.id == id ? {...item,consulted:!item.consulted} : item)
-    setAppointments(updateList)
-  }
-//   console.log(appointments);
-  
+    const updateList = appointments.map((item) =>
+      item.id === id ? { ...item, consulted: !item.consulted } : item
+    );
+    setAppointments(updateList);
+  };
+  //   console.log(appointments);
+
   const handleDelete = (id) => {
-    const filteredList = appointments.filter((item) => item.id !== id)
-    setAppointments(filteredList)
+    const filteredList = appointments.filter((item) => item.id !== id);
+    setAppointments(filteredList);
   };
   return (
     <main className="text-center mt-2">
@@ -28,7 +30,7 @@ export const Home = () => {
       <Doctors doctorData={doctorData} handleAddApp={handleAddApp} />
       <AppointmentList
         appointments={appointments}
-        handleDelete={handleDelete} 
+        handleDelete={handleDelete}
         handleDoubleClick={handleDoubleClick}
       />
     </main>
